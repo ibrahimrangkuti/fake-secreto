35c5b2169a811580ba41fec5ca6bf9c876b585f8
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-11-09 14:44:02 +0700

    ngasih tau mau ngirim pesan ke siapa hehe

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -7,7 +7,7 @@
             <p class="my-3 text-center text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, expedita.</p>
             <form action="{{ route('room.send-message', $room->slug) }}" method="POST">
                 @csrf
-                <input type="text"  name="message" class="w-full mt-3 mb-4 py-2 px-4 ring-1 ring-slate-500 rounded-lg text-[#7743DB] focus:outline-none focus:ring-1 focus:ring-[#7743DB] placeholder:text-slate-500 focus:placeholder:text-[#7743DB] placeholder:text-sm" placeholder="Kasih pesan buat ...">
+                <input type="text"  name="message" class="w-full mt-3 mb-4 py-2 px-4 ring-1 ring-slate-500 rounded-lg text-[#7743DB] focus:outline-none focus:ring-1 focus:ring-[#7743DB] placeholder:text-slate-500 focus:placeholder:text-[#7743DB] placeholder:text-sm" placeholder="Kasih pesan buat {{ $room->name }}">
                 <div class="flex justify-between items-center">
                     <a href="/" class="text-sm">Back to home</a>
                     <button type="submit" class="py-2 px-4 bg-[#7743DB] text-white rounded-lg hover:bg-[#3B3486] ease-in duration-300 float-right">Submit</button>
